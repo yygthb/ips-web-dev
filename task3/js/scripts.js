30eb77f3cde7321c2b2c7249f9b2d573af1d3a28
Yuriy Ya <yy.ainu@gmail.com>
2020-12-29 09:29:46 +0300

    Fix script-file tab = 4 spaces

--- task3/js/scripts.js
+++ task3/js/scripts.js
@@ -3,32 +3,31 @@ const troubleFirst = '.trouble_first'
 const troubleSecond = '.trouble_second'
 const skill = '.skill'
 
-window.addEventListener('load', function () {
-  setBodyLoaded()
-  setAnimatedBlockLoaded(troubleFirst)
-  setAnimatedBlockLoaded(troubleSecond)
-  setAnimatedBlockLoaded(skill)
-})
-
 function setBodyLoaded() {
-  let body = document.querySelector('body')
-  body.classList.add('loaded')
+    let body = document.querySelector('body')
+    body.classList.add('loaded')
 }
 
 function setAnimatedBlockLoaded(selector) {
-  let reached = false
-  const animatedBlock = document.querySelector(selector)
-  const animatedBlockOffset = animatedBlock.offsetTop
-  const windowHeight = window.innerHeight
-
-  window.addEventListener('scroll', function () {
-      let scrolled = window.pageYOffset
-      if (scrolled + windowHeight > animatedBlockOffset + ANIMATED_BLOCK_OFFSET) {
-          if (!reached) {
-              animatedBlock.classList.add('loaded')
-              reached = true
-          }
-
-      }
-  })
+    window.addEventListener('scroll', function () {
+        let reached = false
+        const animatedBlock = document.querySelector(selector)
+        const animatedBlockOffset = animatedBlock.offsetTop
+        const windowHeight = window.innerHeight
+        let scrolled = window.pageYOffset
+        if (scrolled + windowHeight > animatedBlockOffset + ANIMATED_BLOCK_OFFSET) {
+            if (!reached) {
+                animatedBlock.classList.add('loaded')
+                reached = true
+            }
+        }
+    })
 }
+function collectAnimatingBlocks() {
+    setBodyLoaded()
+    setAnimatedBlockLoaded(troubleFirst)
+    setAnimatedBlockLoaded(troubleSecond)
+    setAnimatedBlockLoaded(skill)
+}
+
+window.addEventListener('load', collectAnimatingBlocks)
